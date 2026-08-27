d08d7cdc2dd3acc17f0ab895e19d60a6b1eb02a7
Yamen Bashaireh <bashyamen1@gmail.com>
2026-08-27 20:31:08 +0300

    vars + type conversion + arthmit operation

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,5 +1,70 @@
-import 'package:learn_dart/learn_dart.dart' as learn_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${learn_dart.calculate()}!');
+
+void main(){
+
+   // variables
+  // int , string , double , bool , num , var 
+
+  int age = 18 ; 
+  String name = "Yamen" ; 
+  double gpa = 88.6 ; 
+  bool isUser = true ; 
+bool isUser2 = false;  
+
+print(name) ; 
+
+print(age) ; 
+
+print(isUser2) ; 
+
+
+
+// type conversion 
+
+// String -> int 
+
+String age2 = "18" ; 
+
+int newAge = int.parse(age2) ; 
+print(newAge);
+
+
+// int -> string 
+
+int ageINt = 18 ; 
+String ageText = ageINt.toString(); 
+
+
+// string -> double 
+
+String  ageText2 = "21.5" ; 
+double agedouble = double.parse(ageText2); 
+
+////////////////////  Arthimatec operations  //////////////////////////////////
+
+
+int a = 10 ; 
+int b = 3 ; 
+
+
+print(a+b) ;
+
+print(a-b); 
+print(a*b) ; 
+print(a/b) ; 
+print(a~/b) ; //3.3 ==> 3
+print(a%b); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
